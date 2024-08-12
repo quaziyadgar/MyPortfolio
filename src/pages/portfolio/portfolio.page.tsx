8033afab105ec2e078795140f3175681f6cffaa6
quaziyadgar <quaziyadgar@gmail.com>
2024-08-12 21:20:22 +0530

    Portfolio implemented

--- src/pages/portfolio/portfolio.page.tsx
+++ src/pages/portfolio/portfolio.page.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
+
 import { PageHeaderContent } from "../../components";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { portfolioData } from "../../utils.ts/portfolio";
 import { Link } from "react-router-dom";
 
 import "./portfolio.page.scss";
-import { useState } from "react";
 
 const filterData = [
   {
@@ -23,12 +24,20 @@ const filterData = [
 
 export const Portfolio = () => {
   const [filteredValue, setFilteredValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
 
-  const handleFilter = (id: number) =>{
-    
-  }
+  const handleFilter = (id: number) => {
+    setFilteredValue(id);
+  };
 
-  const filteredItems = filteredValue === 1 ? portfolioData : portfolioData.filter(item=>item.id === filteredValue)
+  const handleHover = (value: any) => {
+    setHoveredValue(value);
+  };
+
+  const filteredItems =
+    filteredValue === 1
+      ? portfolioData
+      : portfolioData.filter((item) => item.id === filteredValue);
 
   return (
     <section id="portfolio" className="portfolio">
@@ -39,21 +48,40 @@ export const Portfolio = () => {
       <div className="portfolio__content">
         <ul className="portfolio__content__filter">
           {filterData.map((item) => (
-            <li className={item.filterId === filteredValue ? 'active' : ''} onClick={handleFilter(item.filterId)} key={item.filterId}>{item.label}</li>
+            <li
+              className={item.filterId === filteredValue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
           ))}
         </ul>
         <div className="portfolio__content__cards">
-          {
-            filteredItems.map((item)=>(
-              <div key={`cardItem${item.name.trim()}`} className="portfolio__content__cards__item">
-                <div className="portfolio__content__cards__item__img-wrapper">
-                  <Link to={item.link}>
-                    <img alt="image" src={item.image}/>
-                  </Link>
-                </div>
+          {filteredItems.map((item, index) => (
+            <div
+              key={`cardItem${item.name.trim()}`}
+              className="portfolio__content__cards__item"
+              onMouseEnter={() => handleHover(index)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <Link to={item.link}>
+                  <img alt="image" src={item.image} />
+                </Link>
               </div>
-            ))
-          }
+              <Link to={item.link} target="_blank">
+                <div className="overlay">
+                  {index === hoveredValue && (
+                    <div>
+                      <p>{item.name}</p>
+                      <button>Visit</button>
+                    </div>
+                  )}
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/utils.ts/portfolio.ts
+++ src/utils.ts/portfolio.ts
@@ -3,13 +3,43 @@ import Alumni from "../assets/portfolioImages/alumni.jpg";
 import Weather from "../assets/portfolioImages/weather-forcast.jpg";
 import TicTacToe from "../assets/portfolioImages/tic-tac-toe.jpg";
 import Timer from "../assets/portfolioImages/timer.jpg";
-import ToDoList from "../assets/portfolioImages/to-do-list.jpg";
+import Workout from "../assets/portfolioImages/workout.jpg";
 
 export const portfolioData = [
-  { id: 2, name: "ECommerce", image: ECommerce, link: "#" },
-  { id: 2, name: "College Alumni", image: Alumni, link: "#" },
-  { id: 2, name: "Tic Tac Toe", image: Weather, link: "#" },
-  { id: 2, name: "Timer", image: TicTacToe, link: "#" },
-  { id: 2, name: "Weather", image: Timer, link: "#" },
-  { id: 3, name: "To do list", image: ToDoList, link: "#" },
+  {
+    id: 2,
+    name: "E-Commerce",
+    image: ECommerce,
+    link: "https://quaziyadgar.github.io/demo_ecom_frontend/",
+  },
+  {
+    id: 2,
+    name: "College Alumni",
+    image: Alumni,
+    link: "https://mmmutalumni-d4d4e.web.app/",
+  },
+  {
+    id: 2,
+    name: "Weather",
+    image: Weather,
+    link: "https://quaziyadgar.github.io/weather/",
+  },
+  {
+    id: 2,
+    name: "Tic Tac Toe",
+    image: TicTacToe,
+    link: "https://quaziyadgar.github.io/16-Jan-Test-Tik-tak-toe/",
+  },
+  {
+    id: 2,
+    name: "Timer",
+    image: Timer,
+    link: "https://quaziyadgar.github.io/02-Feb-Timer/",
+  },
+  {
+    id: 3,
+    name: "Workout",
+    image: Workout,
+    link: "https://quaziyadgar.github.io/www.mygym.com/index.html",
+  },
 ];
