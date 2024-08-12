50ea69775eb5d28c7c14cfbe5888cba6aa560129
quaziyadgar <quaziyadgar@gmail.com>
2024-08-12 21:45:02 +0530

    some fixes for deploying

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaBars, FaReact } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { HiX } from "react-icons/hi";
